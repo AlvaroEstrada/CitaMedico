16a8bcf0db4239b008adca312c74be13e29f1bfe
AlvaroEstrada <alvaroestrada85@gmail.com>
2017-09-25 09:32:28 +0200

    Icono añadido

--- app/src/main/java/com/android/curso/myapplication/MainActivity.java
+++ app/src/main/java/com/android/curso/myapplication/MainActivity.java
@@ -2,14 +2,10 @@ package com.android.curso.myapplication;
 
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioGroup;
@@ -52,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
                 tvhora.setText(hora+"-"+minuto);
             }
-        },10,20,true).show();
+        },10,20,true).show(); // true = formato 24h - false formato 12h
 
 
 
